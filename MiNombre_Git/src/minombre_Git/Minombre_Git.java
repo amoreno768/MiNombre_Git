701a7cfe03807515fab5229337ceaeb57a67b123
usuario <usuario@DESKTOP-DSHUGT9>
2020-05-24 16:52:29 +0200

    Version inical git
    Change-Id: I74fe4911087ad74857dcffec5e3520541e33af8e

---
+++ MiNombre_Git/src/minombre_Git/Minombre_Git.java
@@ -0,0 +1,14 @@
+package minombre_Git;
+
+public class Minombre_Git {
+
+	public static void main(String[] args) {
+
+		System.out.println("\n");
+		System.out.println("¿COMO TE LLAMAS?");
+
+		System.out.println("\n");
+		System.out.println("HOLA, ME LLAMO  'ALEJANDRO MORENO LÓPEZ'");
+	}
+
+}
